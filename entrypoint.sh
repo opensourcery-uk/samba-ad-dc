8ab102a3a73267ee568d478e140cc88e68750d53
Open Sourcery <open.source@opensourcery.uk>
2019-08-02 14:25:03 +0100

    Since /etc/krb5.conf isn't persisted, we just copy it out of where it is persisted after domain creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,10 @@ if [ "$#" -eq 0 ]; then
   if [[ ! -f /etc/samba/smb.conf ]]; then
     echo "No samba config exists, provisioning new samba domain"
     samba-tool domain provision --use-rfc2307 --domain=${SAMBA_DOMAIN} --realm=${SAMBA_REALM} --server-role=dc --dns-backend=BIND9_DLZ --adminpass=${SAMBA_DOMAIN_PASSWORD} --option "bind interfaces only = yes" --option "interfaces = lo ${NET_DEV}" --option "log file = /var/log/samba/%m.log" --option "max log size = 10000"
-    cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
-    chgrp bind /etc/krb5.conf
   fi
 
+  cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
+  chgrp bind /etc/krb5.conf
   /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n -e debug
 else
   eval ${@}
