8d0e9dc9400781f4b9ed3b63f2327fb570d1c2fb
Open Sourcery <open.source@opensourcery.uk>
2019-08-02 12:11:02 +0100

    with unbound variable checking enabled you can't check it a variable is null

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 ip=$(ip addr show dev ${NET_DEV} | grep inet | awk '{ print $2 }' | awk -F '/' '{ print $1 }')
 echo "${ip} $(hostname).${SAMBA_REALM}" >> /etc/hosts
 
-if [ -z "${1}" ]; then
+if [ "$#" -eq 0 ]; then
   if [[ ! -f /etc/samba/smb.conf ]]; then
     echo "No samba config exists, provisioning new samba domain"
     samba-tool domain provision --use-rfc2307 --domain=${SAMBA_DOMAIN} --realm=${SAMBA_REALM} --server-role=dc --dns-backend=BIND9_DLZ --adminpass=${SAMBA_DOMAIN_PASSWORD} --option "bind interfaces only = yes" --option "interfaces = lo ${NET_DEV}" --option "log file = /var/log/samba/%m.log" --option "max log size = 10000"
